b97d64f389ddb0d20273c25c9fe9ee36f988bb6b
tessssssssy <tessfabry@gmail.com>
2020-07-15 14:24:52 +1000

    add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hidden Gems</h1>
-    </div>
+    <Switch>
+        <Route exact path="/" component={Home} />
+        <ProtectedRoute exact path="/countries" component={CountriesList} />
+        <ProtectedRoute exact path="/countries/create" component={CreateCountry} />
+        <Route exact path="/countries/:name" component={Country}/>
+        <Route exact path="/login" render={(props) => <Login {...props} setLoggedIn={setLoggedIn}/> } />
+        <Route exact path="/sign-up" render={(props)=> <SignUp {...props} setLoggedIn={setLoggedIn}/>} />    
+      </Switch>
   );
 }
 

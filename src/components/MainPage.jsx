debab00ebd7db26724693765ed2a0780743845de
Joyce <nokjo86@hotmail.com>
2020-07-30 00:33:04 +1000

    updated event listenter on toggler from onSelectCapture to OnClick to trigger setState

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -80,7 +80,7 @@ class MainPage extends React.Component {
       });
       locations = savedLocation;
     }
-
+    console.log(locations)
     const positionFixed = { position: "fixed" };
 
     return (
@@ -106,7 +106,7 @@ class MainPage extends React.Component {
               </div>
               {sessionStorage.getItem("currentUser") && (
                 <div class="ui toggle checkbox">
-                  <input type="checkbox" onSelectCapture={this.onToggle}></input>
+                  <input type="checkbox" onClick={this.onToggle}></input>
                   <label>Show Saved Locations</label>
                 </div>
               )}             
